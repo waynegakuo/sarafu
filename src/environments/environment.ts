51c433af72ebe29e1665300690259e29f93b1285
Wayne Gakuo <waynejotham@gmail.com>
2023-05-29 16:35:47 +0300

    feature(project): set up firebase hosting and GitHub Actions on GitHub upon commit push, add environments file to handle Firebase-related functionalities

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAjcRVBeV1mfcL3pksBLVA4UGymHkFTSJ0",
+    authDomain: "sarafu-app.firebaseapp.com",
+    projectId: "sarafu-app",
+    storageBucket: "sarafu-app.appspot.com",
+    messagingSenderId: "509052933457",
+    appId: "1:509052933457:web:6461a01ded165a49b92f70",
+    measurementId: "G-40ZEK6F12Y"
+  }
+};
