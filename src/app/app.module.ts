3fce9b9b9d3985d25dd1fce0c876c6b97ade6d4f
Wayne Gakuo <30318171+waynegakuo@users.noreply.github.com>
2023-05-29 16:12:14 +0300

    feature(project): landing page (#2)
    * feature(project): create navbar component and implement initial UI, use custom favicon, add assets for landing page, use Lato font family
    
    * feature(project): implement landing page UI, install Angular Material, move files for proper architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
